4889d60bc181612a8a091e0bb7ce6c6dffe626ca
Gitnewbit <snatural6@gmail.com>
2026-05-03 15:29:11 +0200

    fix: correct vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/",   // 🔥 THIS IS THE FIX
   plugins: [react()],
-  base: process.env.VITEBASE_PATH || "/IntelliSupport"
-});
+})
